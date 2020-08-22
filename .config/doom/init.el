fa2beca1f61a3bf3bcba3f6496784d15d6032107
wacken <sebastianwalchi@gmail.com>
2020-08-23 01:09:45 +0200

    use normal dired and clojure mode

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -64,7 +64,7 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       (dired +ranger)             ; making dired pretty [functional]
+       dired              ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
@@ -106,8 +106,8 @@
 
        :lang
        ;;agda              ; types of types of types of types...
-       ;;cc                ; C/C++/Obj-C madness
-       (clojure +lsp)            ; java with a lisp
+       ;;cc               ; C/C++/Obj-C madness
+       clojure            ; java with a lisp
        common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
