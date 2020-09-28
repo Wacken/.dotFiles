78184ecc29d12cb567af1a76545ecf899be43e44
wacken <sebastianwalchi@gmail.com>
2020-09-28 23:55:27 +0200

    Some further doom macs configs with elfeed, snippets, org-bullets and more

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -49,4 +49,7 @@
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
 
+(package! company-statistics)
+(package! yasnippet-snippets)
+(package! org-bullets)
 (package! elfeed-goodies)
